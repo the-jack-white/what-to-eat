926c8c6fafc5b1fb9b1b427c3e8cbebb2c1e81d7
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-07-09 21:27:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$('.main-button').on('mouseover', () => {
-    console.log("I am hovering")
-})
+const submitButton = document.querySelector('.submit-button');
+const restaurantDescription = document.querySelector('.restaurant-description'); 
+const restaurantName = document.querySelector('.restaurant-name');
+
+
+// Async function to get the data from the JSON file
+async function getData() {
+    const response = await fetch('../data/restaurants.json');
+    const data = await response.json();
+    const random = Math.floor(Math.random() * Math.floor(data.length));
+    const randomRestaurant = data[random];
+    console.log(randomRestaurant);
+
+    restaurantName.textContent = randomRestaurant.name;
+}
+
+submitButton.addEventListener('click', () => {
+    getData();
+});
