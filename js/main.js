efe05d77867abe239927e5e89c5c11b1db594dee
bridge2digital <bridge2digitalv2@gmail.com>
2019-07-10 14:44:11 +0200

    Toggle list on and off

--- js/main.js
+++ js/main.js
@@ -4,14 +4,15 @@ const restaurantDescription = document.querySelectorAll('.restaurant-description
 const restaurantName = document.querySelector('.restaurant-name');
 const imgParent = document.querySelectorAll('.img')[0];
 
-
+const ul = document.createElement('ul');
+restaurantDescription.appendChild(ul);
 
 // Create elements for pickForMe() 
 const h3 = document.createElement('h3');
 const span = document.createElement('span');
 const img = document.createElement('img');
 
-
+let showList = false;
 
 
 // Async function to get the data from the JSON file
@@ -19,10 +20,24 @@ async function getData() {
     const response = await fetch('../data/restaurants.json');
     const data = await response.json();
 
+    // Display all the restaurants in the JSON and hide it in the DOM
+    listAll(data);
+    ul.style.display = 'none';
+
     // Call the display all function to display a list of all the restaurants
     allButton.addEventListener('click', () => {
-        listAll(data);
-    })
+        if (!showList) {
+            showList = true;
+            allButton.textContent = "HIDE ALL RESTAURANTS";
+            toggleListOn();
+            
+        } else {
+            showList = false;
+            allButton.textContent = "SHOW ALL RESTAURANTS";
+            toggleListOff();
+        }
+        
+    });
 
     // Call the random generator if the 'PICK RESTAURANT FOR ME' button is clicked
     pickButton.addEventListener('click', () => {
@@ -32,17 +47,20 @@ async function getData() {
 
 // List all the restaurant names in the JSON
 function listAll(data) {
-    // Create parent element
-    const ul = document.createElement('ul');
-    
-    restaurantDescription.appendChild(ul);
-
     for (let i = 0; i < data.length; i ++) {
         // create <li> element inside loop so a new list item is created for each data item
         let li = document.createElement('li');
         li.textContent = data[i].name;
         ul.appendChild(li);
     }
+}
+
+function toggleListOn() {
+    ul.style.display = 'block';
+}
+
+function toggleListOff() {
+    ul.style.display = 'none';
 }
 
 // Generate a random number and pick the corresponding JSON index
@@ -53,6 +71,7 @@ function pickForMe(data) {
 
     // Return restaurant name
     h3.textContent = randomRestaurant.name;
+    h3.className = "restaurant-name";
     restaurantDescription.appendChild(h3);
 
     // Set <span> element and append to parent(restaurantDescription)
